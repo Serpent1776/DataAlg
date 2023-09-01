ff3fe734489689ac51b9f988c9200487f0f14dcf
Serpent1776 <j.d.hemling@email.msmary.edu>
2023-09-01 09:17:31 -0400

    Homework 1 finished

--- Homework/Hw1/src/Sign.java
+++ Homework/Hw1/src/Sign.java
@@ -6,16 +6,16 @@ public class Sign { //Ap question 2 for problem I.
        this.message = message;
        this.width = width;
     }
-    public int numberOfLines() {
+    public int numberOfLines() { //finds the number of lines in the sign, rounded
          return Math.round(message.length()/((float)(width)));
     }
-    public String getLines() {
+    public String getLines() { //prints where the line distinctions are, noted by a semicolon.
         if(message.length() == 0) {return null;} else {
             int speed = numberOfLines();
             if(speed > 1) {
                 String linedMessage = "";
                 String[] parts = new String[speed];
-                for(int i = 0; i < speed; i++) {
+                for(int i = 0; i < speed; i++) { //breaking into parts and semi colon insertion (into those parts) loop
                         if(width*(i+1) < message.length()) {
                             parts[i] = message.substring(width*i, width*(i+1));
                             parts[i] += ";";
@@ -24,7 +24,7 @@ public class Sign { //Ap question 2 for problem I.
                             parts[i] += ";";
                         }
                 }
-                for(int i = 0; i < parts.length; i++) {
+                for(int i = 0; i < parts.length; i++) { //sums all the parts back up.
                         linedMessage += parts[i];
                     }
                 return linedMessage;
