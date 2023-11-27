439773318e8faef1a1e9e8bcfe340a9ca798341a
Serpent1776 <j.d.hemling@email.msmary.edu>
2023-11-27 09:25:23 -0500

    app2 scannerization

--- Play Around/playground again/src/App2.java
+++ Play Around/playground again/src/App2.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class App2 {
     public static void main(String[] args) {
-        String a = "into";
-        String b = "lowercase";
-        System.out.println(a.compareTo(b));
+        Scanner typo = new Scanner(System.in);
+        System.out.print("First one: ");
+        String a = typo.next(); 
+        System.out.print("Second one: ");
+        String b = typo.next();
+        System.out.println("Comparision: " + compo(a, b));
+    }
+    public static String compo(String a, String b) {
+        int comp = a.compareTo(b);
+        if(comp > 0) {
+            return "first > second";
+        } else if (comp < 0) {
+            return "first < second";
+        } else {
+            return "first = second";
+        }
     }
 }
